cec23e4fce2ce95d664cad322b9028fb616a1a28
Claudiu_1503 <adichis1503@yahoo.com>
2024-10-04 18:55:03 +0300

    full 308

---
+++ src/main/java/guru/springframework/reactivemongo/web/fn/BeerHandler.java
@@ -0,0 +1,26 @@
+package guru.springframework.reactivemongo.web.fn;
+
+import guru.springframework.reactivemongo.model.BeerDTO;
+import guru.springframework.reactivemongo.services.BeerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+@RequiredArgsConstructor
+public class BeerHandler {
+    private final BeerService beerService;
+
+    public Mono<ServerResponse> getBeerById(ServerRequest request){
+        return ServerResponse
+                .ok()
+                .body(beerService.getById(request.pathVariable("beerId")), BeerDTO.class);
+    }
+
+    public Mono<ServerResponse> listBeers(ServerRequest request){
+        return ServerResponse.ok()
+                .body(beerService.listBeers(), BeerDTO.class);
+    }
+}
